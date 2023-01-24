1ad1ed65654bbb26a84a2413f0a464c071054cdc
Paige Gulley <nano3.14@gmail.com>
2023-01-24 18:48:54 -0500

    installation location

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl -SL https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin -o lid.176.bin
-mv lid.176.bin providers/language 
+mv lid.176.bin mc-providers/language 
