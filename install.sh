ff399e2c8ec665cf3ff3362b20748cf5cdd1769d
Paige Gulley <nano3.14@gmail.com>
2023-01-24 19:07:14 -0500

    moving and movin

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 curl -SL https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin -o lid.176.bin
-mv lid.176.bin mc-providers/language 
+mv lid.176.bin mc_providers/language 
 
